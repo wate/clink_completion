705bfaf9001be2609350e6a48344d776d031e9b4
wate <pc.poisoning@gmail.com>
2013-09-16 14:32:02 +0900

    compassコマンド修正中

--- compass.lua
+++ compass.lua
@@ -1,10 +1,7 @@
-local compass_env_argument_tree = {'development', 'production'}
-local compass_output_style_argument_tree = {'nested', 'expanded', 'compact', 'compressed'}
+local compass_argument_tree = {'create', 'clean', 'compile', 'init', 'watch', 'config', 'extension', 'frameworks', 'grid-img', 'imports', 'install', 'interactive', 'sprite', 'stats', 'unpack', 'validate', 'version', 'help'}
 
-local compass_argument_tree = {
-	'clean',
-	'compile',
-	create = {
+--[[
+local compass_create_argument_tree = {
 			'--using',
 			'-x', '--syntax',
 			'--prepare',
@@ -30,39 +27,25 @@ local compass_argument_tree = {
 			'--no-line-comments',
 			'--bare',
 			'-?', '-h', '--help'
-		},
-	'init',
-	'watch',
-	'config',
-	'extension',
-	'frameworks',
-	'grid-img',
-	'imports',
-	'install',
-	'interactive',
-	'sprite',
-	'stats',
-	'unpack',
-	'validate',
-	'version',
-	help = {
-		'clean',
-		'compile',
-		'create',
-		'init',
-		'watch',
-		'config',
-		'extension',
-		'frameworks',
-		'grid-img',
-		'imports',
-		'install',
-		'interactive',
-		'sprite',
-		'stats',
-		'unpack',
-		'validate',
-		'version'
-	}	
 }
+local compass_environment_argument_tree = {'development', 'production'}
+compass_create_argument_tree['-e'] = clink.arg.tree_node("*", compass_environment_argument_tree)
+compass_create_argument_tree['--environment'] = clink.arg.tree_node("*", compass_environment_argument_tree)
+local compass_output_style_argument_tree = {'nested', 'expanded', 'compact', 'compressed'}
+compass_create_argument_tree['-s'] = clink.arg.tree_node("*", compass_output_style_argument_tree)
+compass_create_argument_tree['--output-style'] = clink.arg.tree_node("*", compass_output_style_argument_tree)
+compass_argument_tree['create'] = clink.arg.tree_node("*", compass_create_argument_tree)
+
+local function compass_argument_tree(text, first, last){
+	local leading = rl_line_buffer:sub(1, first - 1)
+	local environment = clink.arg.node_transpose({'-e', '--environment'}, {'development', 'production'})
+	local outputStyle = clink.arg.node_transpose({'-s', '--output-style'}, {'nested', 'expanded', 'compact', 'compressed'})
+	
+	local subCommans = {'clean', 'compile', 'init', 'watch', 'config', 'extension', 'frameworks', 'grid-img', 'imports', 'install', 'interactive', 'sprite', 'stats', 'unpack', 'validate', 'version', 'help'}
+	local subCommans['help'] = subCommans;
+	
+	
+	return true;
+}
+]]
 clink.arg.register_tree("compass", compass_argument_tree)
