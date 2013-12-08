e3f49e6726f67e95cd32610fbb94a4b53be9a142
wate <pc.poisoning@gmail.com>
2013-12-08 22:27:39 +0900

    各プラグインの機能を可変に出来るようにコードを分離

--- completion/vagrant.lua
+++ completion/vagrant.lua
@@ -1,3 +1,14 @@
+--------------------------------------------------------
+-- Vagrant Plugins
+--------------------------------------------------------
+sahara = true			-- sahara
+global_status = true	-- global-status
+multi_putty = false		-- vagrant-multi-putty
+vbox_snapshot = false	-- vagrant-vbox-snapshot
+
+--------------------------------------------------------
+-- vagrant
+--------------------------------------------------------
 local vagrant_parser = clink.arg.new_parser()
 vagrant_parser:set_arguments({
 	"box" .. clink.arg.new_parser():set_arguments({'add', 'list', 'remove', 'repackage'}),
@@ -13,12 +24,54 @@ vagrant_parser:set_arguments({
 	'ssh-config',
 	'status',
 	'suspend',
-	'up',
-	-- sahara plugin
-	"sandbox" .. clink.arg.new_parser():set_arguments({'on', 'rollback', 'commit', 'status', 'off'}),
-	-- global-status plugin
-	"global-status",
-	-- PuTTY plugin
-	"putty"
+	'up'
 	})
 clink.arg.register_parser("vagrant", vagrant_parser)
+--------------------------------------------------------
+-- sahara
+--------------------------------------------------------
+if sahara then
+	local vagrant_sahara_parser = clink.arg.new_parser()
+	vagrant_sahara_parser:set_arguments({
+		"sandbox"..clink.arg.new_parser():set_arguments({
+			'on',
+			'rollback',
+			'commit',
+			'status',
+			'off'
+		})
+	})
+	clink.arg.register_parser("vagrant", vagrant_sahara_parser)
+end
+--------------------------------------------------------
+-- global-status
+--------------------------------------------------------
+if global_status then
+	local vagrant_global_status_parser = clink.arg.new_parser()
+	vagrant_global_status_parser:set_arguments({"global-status"})
+	clink.arg.register_parser("vagrant", vagrant_global_status_parser)
+end
+--------------------------------------------------------
+-- vagrant-multi-putty
+--------------------------------------------------------
+if multi_putty then
+	local vagrant_multi_putty_parser = clink.arg.new_parser()
+	vagrant_multi_putty_parser:set_arguments({"putty"})
+	clink.arg.register_parser("vagrant", vagrant_multi_putty_parser)
+end
+--------------------------------------------------------
+-- vagrant-vbox-snapshot
+--------------------------------------------------------
+if vbox_snapshot then
+	local vagrant_vbox_snapshot_parser = clink.arg.new_parser()
+	vagrant_vbox_snapshot_parser:set_arguments({
+		"snapshot"..clink.arg.new_parser():set_arguments({
+			'take',
+			'list',
+			'back',
+			'delete',
+			'go'
+		})
+	})
+	clink.arg.register_parser("vagrant", vagrant_vbox_snapshot_parser)
+end
