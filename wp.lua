774b4be7c1c70bf32a278509e74394182d30264d
wate <pc.poisoning@gmail.com>
2014-05-09 04:26:22 +0900

    コメントを削除

--- wp.lua
+++ wp.lua
@@ -386,33 +386,33 @@ wp_parser:set_flags(
 	"--help"
 )
 wp_parser:set_arguments({
-	"cache"..wp_cache_parser,					-- Manage the object cache.
-	"cap"..wp_cap_parser,						-- Manage user capabilities.
-	"cli"..wp_cli_parser,						-- Get information about WP-CLI itself.
-	"comment"..wp_comment_parser,				-- Manage comments.
-	"core"..wp_core_parser,						-- Download, install, update and otherwise manage WordPress proper.
-	"db"..wp_db_parser,							-- Perform basic database operations.
-	"eval"..wp_eval_parser,						-- Execute arbitrary PHP code after loading WordPress.
-	"eval-file"..wp_eval_file_parser,			-- Load and execute a PHP file after loading WordPress.
-	"export"..wp_export_parser,					-- Export content to a WXR file.
-	"import"..wp_import_parser,					-- Import content from a WXR file.
-	"media"..wp_media_parser,					-- Manage attachments.
-	"network-meta"..wp_network_meta_parser,		-- Manage network custom fields.
-	"option"..wp_option_parser,					-- Manage options.
-	"plugin"..wp_plugin_parser,					-- Manage plugins.
-	"post"..wp_post_parser,						-- Manage posts.
-	"post-meta"..wp_post_meta_parser,			-- Manage post custom fields.
-	"rewrite"..wp_rewrite_parser,				-- Manage rewrite rules.
-	"role"..wp_role_parser,						-- Manage user roles.
-	"scaffold"..wp_scaffold_parser,				-- Generate code for post types, taxonomies, etc.
-	"search-replace"..wp_search_replace_parser,	-- Search/replace strings in the database.
-	"shell"..wp_shell_parser,					-- Interactive PHP console.
-	"site"..wp_site_parser,						-- Perform site-wide operations.
-	"term"..wp_term_parser,						-- Manage terms.
-	"theme"..wp_theme_parser,					-- Manage themes.
-	"transient"..wp_transient_parser,			-- Manage transients.
-	"user"..wp_user_parser,						-- Manage users.
-	"user-meta"..wp_user_meta_parser,			-- Manage user custom fields.
-	"help"..wp_help_parser,						-- Get help on a certain command.
+	"cache"..wp_cache_parser,
+	"cap"..wp_cap_parser,
+	"cli"..wp_cli_parser,
+	"comment"..wp_comment_parser,
+	"core"..wp_core_parser,
+	"db"..wp_db_parser,
+	"eval"..wp_eval_parser,
+	"eval-file"..wp_eval_file_parser,
+	"export"..wp_export_parser,
+	"import"..wp_import_parser,
+	"media"..wp_media_parser,
+	"network-meta"..wp_network_meta_parser,
+	"option"..wp_option_parser,
+	"plugin"..wp_plugin_parser,
+	"post"..wp_post_parser,
+	"post-meta"..wp_post_meta_parser,
+	"rewrite"..wp_rewrite_parser,
+	"role"..wp_role_parser,
+	"scaffold"..wp_scaffold_parser,
+	"search-replace"..wp_search_replace_parser,
+	"shell"..wp_shell_parser,
+	"site"..wp_site_parser,
+	"term"..wp_term_parser,
+	"theme"..wp_theme_parser,
+	"transient"..wp_transient_parser,
+	"user"..wp_user_parser,
+	"user-meta"..wp_user_meta_parser,
+	"help"..wp_help_parser,
 })
 clink.arg.register_parser("wp", wp_parser)
