25bc60d0cf812b055550a7e8dc0fd84624e0e270
wate <pc.poisoning@gmail.com>
2014-05-27 04:52:33 +0900

    Revert "コメントおよびタブ削除"
    This reverts commit fec2174e5d4732920f6b7ea8ad17c72843dd3b3c.

--- compass.lua
+++ compass.lua
@@ -5,62 +5,62 @@ compass_output_style_arguments:set_arguments({"nested", "expanded", "compact", "
 local compass_syntax_arguments = clink.arg.new_parser()
 compass_syntax_arguments:set_arguments({"scss", "sass"})
 local compass_common1_flags = {
-"-r", "--require",
-"-l", "--load",
-"-L", "--load-all",
-"-I", "--import-path",
-"-q", "--quiet",
-"--trace",
-"--force",
-"--dry-run",
-"--boring",
-"-?", "-h", "--help"
+	"-r", "--require",
+	"-l", "--load",
+	"-L", "--load-all",
+	"-I", "--import-path",
+	"-q", "--quiet",
+	"--trace",
+	"--force",
+	"--dry-run",
+	"--boring",
+	"-?", "-h", "--help"
 }
 local compass_common2_flags = {
-"-e" .. compass_environment_arguments,
-"--environment" .. compass_environment_arguments,
-"-s" .. compass_output_style_arguments,
-"--output-style" .. compass_output_style_arguments,
-"-c", "--config",
-"--app",
-"--app-dir",
-"--sass-dir" ,
-"--images-dir",
-"--javascripts-dir",
-"--fonts-dir",
-"--relative-assets",
-"--no-line-comments",
-"--http-path",
-"--generated-images-path"
+	"-e" .. compass_environment_arguments,
+	"--environment" .. compass_environment_arguments,
+	"-s" .. compass_output_style_arguments,
+	"--output-style" .. compass_output_style_arguments,
+	"-c", "--config",
+	"--app",
+	"--app-dir",
+	"--sass-dir" ,
+	"--images-dir",
+	"--javascripts-dir",
+	"--fonts-dir",
+	"--relative-assets",
+	"--no-line-comments",
+	"--http-path",
+	"--generated-images-path"
 }
 local compass_common_parser = clink.arg.new_parser()
 compass_common_parser:set_flags(compass_common1_flags, compass_common2_flags);
 local compass_create_parser = clink.arg.new_parser()
 compass_create_parser:set_flags(
-compass_common1_flags,
-compass_common2_flags,
-"--using",
-"-x" .. compass_syntax_arguments,
-"--syntax" .. compass_syntax_arguments,
-"--prepare",
-"--bare"
-)
+	compass_common1_flags,
+	compass_common2_flags,
+	"--using",
+	"-x" .. compass_syntax_arguments,
+	"--syntax" .. compass_syntax_arguments,
+	"--prepare",
+	"--bare"
+	)
 local compass_init_parser = clink.arg.new_parser()
 compass_init_parser:set_flags(
-compass_common1_flags,
-compass_common2_flags,
-"--using",
-"-x" .. compass_syntax_arguments,
-"--syntax" .. compass_syntax_arguments,
-"--prepare"
-)
+	compass_common1_flags,
+	compass_common2_flags,
+	"--using",
+	"-x" .. compass_syntax_arguments,
+	"--syntax" .. compass_syntax_arguments,
+	"--prepare"
+	)
 local compass_install_parser = clink.arg.new_parser()
 compass_install_parser:set_flags(
-compass_common1_flags,
-compass_common2_flags,
-"-x" .. compass_syntax_arguments,
-"--syntax" .. compass_syntax_arguments
-)
+	compass_common1_flags,
+	compass_common2_flags,
+	"-x" .. compass_syntax_arguments,
+	"--syntax" .. compass_syntax_arguments
+	)
 local compass_compile_parser = clink.arg.new_parser()
 compass_compile_parser:set_flags(compass_common1_flags, compass_common2_flags, "--time", "--debug-info", "--no-debug-info")
 local compass_watch_parser = clink.arg.new_parser()
@@ -73,28 +73,28 @@ local compass_version_parser = clink.arg.new_parser()
 compass_version_parser:set_flags("-q", "--quiet", "--major", "--minor", "--patch", "--revision", "-?", "-h", "--help")
 local compass_parser = clink.arg.new_parser()
 compass_parser:set_arguments({
-"clean" .. compass_common_parser,
-"compile" .. compass_compile_parser,
-"create" .. compass_create_parser,
-"init" .. compass_init_parser,
-"watch" .. compass_watch_parser,
-"config" .. compass_config_parser,
-extension = {"install", "uninstall", "list"},
-"frameworks" .. clink.arg.new_parser():set_flags(compass_common1_flags),
-"imports",
-"install" .. compass_install_parser,
-"interactive" .. compass_common_parser,
-"sprite" .. compass_sprite_parser,
-"stats" .. compass_common_parser,
-"unpack" .. compass_common_parser,
-"validate" .. compass_common_parser,
-"version" .. compass_version_parser
-})
+	"clean" .. compass_common_parser,
+	"compile" .. compass_compile_parser,
+	"create" .. compass_create_parser,
+	"init" .. compass_init_parser,
+	"watch" .. compass_watch_parser,
+	"config" .. compass_config_parser,
+	extension = {"install", "uninstall", "list"},
+	"frameworks" .. clink.arg.new_parser():set_flags(compass_common1_flags),
+	"imports",
+	"install" .. compass_install_parser,
+	"interactive" .. compass_common_parser,
+	"sprite" .. compass_sprite_parser,
+	"stats" .. compass_common_parser,
+	"unpack" .. compass_common_parser,
+	"validate" .. compass_common_parser,
+	"version" .. compass_version_parser
+	})
 local compass_help_parser = clink.arg.new_parser()
 compass_help_parser:set_arguments({
-"help" .. clink.arg.new_parser():set_arguments({
-compass_parser:flatten_argument(1)
-})
+    "help" .. clink.arg.new_parser():set_arguments({
+    	compass_parser:flatten_argument(1)
+    	})
 })
 clink.arg.register_parser("compass", compass_parser)
 clink.arg.register_parser("compass", compass_help_parser)
